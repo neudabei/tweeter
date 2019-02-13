078f55aa752fcc18a1a4088d2eb4f9f69ad8dcf5
robertsj <rschultzejena@gmail.com>
2019-02-13 20:31:06 +0100

    Create main script to execute froom command line
    This script calls the tweeter class. It is simple and untested.
    
    Initializing the class and calling the `Twitter#tweet` instance method
    directly outisde the class from within the same file caused Twitter to
    be called each time Rspec was run, despite stubbing the API.
    
    Some ways to call the script are:
    
    * ./main.rb text.md
    * cat text.md | ./main.rb
    * echo "hello twitter" | ./main.rb
    
    Note that `echo` will add a newline to the last tweet, if not used in combination with the
    `-n` flag, e.g. `$ echo -n "hello world"`.

--- main.rb
+++ main.rb
@@ -1,38 +1,6 @@
 #!/usr/bin/env ruby
 
-class Tweeter
-  TWEET_LENGTH = 280
-
-  attr_reader :tweets
-
-  def initialize
-    @tweets = Array.new
-  end
-
-  def parse_input
-    input = ARGF.read
-
-    until input.size <= TWEET_LENGTH do
-      index_of_last_ocurrence_of_space = input.rindex(' ', TWEET_LENGTH)
-      tweet = input.slice!(0, index_of_last_ocurrence_of_space)
-      tweet.lstrip!
-
-      @tweets << tweet
-    end
-
-    @tweets << input.lstrip
-  end
-
-  def add_numbering_to_list
-    if @tweets.count > 1
-
-      @tweets = @tweets.map.with_index do |element, index|
-        "#{index + 1}" +  ') ' + element.to_s
-      end
-    end
-  end
-end
+require_relative 'tweeter'
 
 tweeter = Tweeter.new
-tweeter.parse_input
-tweeter.add_numbering_to_list
+tweeter.tweet
